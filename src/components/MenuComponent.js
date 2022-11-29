a8a8faf395f4f2d78aaafe907683123d9c8eea92
MohamedM7 <mammeri244@gmail.com>
2022-11-29 04:43:23 +0300

    adding the reactstrap components card cardImd and rendering the onClick function

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -5,15 +5,43 @@ class Menu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          
+          selectedDish:null
         };
+    }
+
+    onDishSelect(dish){
+      this.setState({selectedDish:dish})
+    }
+
+    renderDish(dish){
+      if(dish != null){
+        return(
+           <Card>
+             <CardImg width="100%" object src={dish.image} alt={dish.name} />
+             <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+             </CardBody>
+           </Card>
+
+        );
+
+      }
+      else{
+         return(
+          <div></div> /*this equal to return null*/ 
+         );
+
+      }
+
+
     }
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                <div key={dish.id} className="col-12 col-md-5 m-1"> {/* col-12 small and extra small scren , col-md-5 for the medium */}
-                <Card>
+                <Card onClick={()=>this.onDishSelect(dish)}>
                   <CardImg width="100%" object src={dish.image} alt={dish.name} />
                   <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -27,6 +55,9 @@ class Menu extends Component {
           <div className="container">
             <div className="row">
                   {menu}
+            </div>
+            <div className="row">
+             {this.renderDish(this.state.selectedDish)}
             </div>
           </div>
         );
